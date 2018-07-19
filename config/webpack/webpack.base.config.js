04aef67cb7cdbdcb202a01fbd5dfa658056859c7
simbawu <code@simbawu.com>
2018-07-19 15:42:09 +0800

    add webpack config

---
+++ config/webpack/webpack.base.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: ['./src/main.js']
+  },
+  output: {
+    path: path.resolve(__dirname, '../../dist'),
+    filename: '[name].js',
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|build)/,
+        query: {
+          presets: ['env']
+        }
+      }, {
+        test: /\.scss$/,
+        use: [{
+          loader: "style-loader"
+        }, {
+          loader: "css-loader",
+          options: {
+            modules: true,
+            localIdentName: '[name]_[local]_[hash:base64:3]'
+          },
+        }, {
+          loader: "sass-loader"
+        }]
+      }, {
+        test: /\.css$/,
+        use: [{
+          loader: "style-loader"
+        }, {
+          loader: "css-loader"
+        }, {
+          loader: "sass-loader"
+        }]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({ // 定义环境变量
+      "process.env": JSON.stringify(process.env.NODE_ENV)
+    }),
+    new HtmlWebpackPlugin({
+      filename: './index.html',
+      template: './public/index.html',
+      inject: "body",
+      minify: {
+        caseSensitive: false,
+        collapseBooleanAttributes: true,
+        collapseWhitespace: true
+      },
+      hash: true,
+      chunks: "app"
+    })
+  ],
+  resolve: {
+    alias: {
+      components: path.join(__dirname, '../../components'),
+      actions: path.join(__dirname, '../../src/actions')
+    }
+  }
+};
