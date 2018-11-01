7eddd9efa2f40b8c0d547d4a9bd0253d50f7345a
simbawu <code@simbawu.com>
2018-11-01 15:33:45 +0800

    chore: update babel config

--- config/webpack/webpack.base.config.js
+++ config/webpack/webpack.base.config.js
@@ -17,7 +17,10 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /(node_modules|build)/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
         }
       }, {
         test: /\.scss$/,
