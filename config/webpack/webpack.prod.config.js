4ef6477691043bdba4ee5b58c56521a4b9181d64
simbawu <code@simbawu.com>
2018-11-01 10:08:50 +0800

    chore: add uglifyjs-webpack-plugin

--- config/webpack/webpack.prod.config.js
+++ config/webpack/webpack.prod.config.js
@@ -1,5 +1,16 @@
-const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-
+  mode: 'production',
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          compress: {
+            drop_console: true
+          }
+        }
+      })
+    ]
+  }
 };
