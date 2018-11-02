54dccef46f508eee72da71d55cc3142acfbb8a5c
simbawu <code@simbawu.com>
2018-11-03 00:01:30 +0800

    chore: update selectorBlackList

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -8,7 +8,7 @@ module.exports = {
       viewportHeight: 1334, // (Number) The height of the viewport.
       unitPrecision: 3, // (Number) The decimal numbers to allow the REM units to grow to.
       viewportUnit: 'vw', // (String) Expected units.
-      selectorBlackList: ['.ignore', '.hairlines'], // (Array) The selectors to ignore and leave as px.
+      selectorBlackList: [/[a-zA-Z0-9]+Ignore$/], // (Array) The selectors to ignore and leave as px.
       minPixelValue: 1, // (Number) Set the minimum pixel value to replace.
       mediaQuery: false // (Boolean) Allow px to be converted in media queries.
     })
