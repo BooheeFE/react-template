9efa53106ca5907b75102c082846db802c898029
simbawu <code@simbawu.com>
2018-11-16 16:58:13 +0800

    refactor: change the react suffix to jsx

--- config/webpack/webpack.base.config.js
+++ config/webpack/webpack.base.config.js
@@ -10,7 +10,7 @@ const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   entry: {
-    app: ['./src/main.js']
+    app: ['./src/main']
   },
   output: {
     path: path.resolve(__dirname, '../../dist'),
