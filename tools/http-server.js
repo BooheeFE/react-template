f691d111903a7fbc0668354ec8b2dfc04b1a2052
simbawu <code@simbawu.com>
2019-01-15 09:31:48 +0800

    style: format code

--- tools/http-server.js
+++ tools/http-server.js
@@ -3,21 +3,27 @@
  * @Author: simbawu
  * @Date: 2018-11-26 19:01:53
  * @LastEditors: simbawu
- * @LastEditTime: 2018-12-05 20:42:23
+ * @LastEditTime: 2019-01-22 17:54:40
  */
 import axios from 'axios';
 
-axios.interceptors.request.use(config => {
-  return config;
-}, error => {
-  return Promise.reject(error);
-});
+axios.interceptors.request.use(
+  config => {
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
 
-axios.interceptors.response.use(response => {
-  return response;
-}, error => {
-  return Promise.reject(error.response);
-});
+axios.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    return Promise.reject(error.response);
+  }
+);
 
 function errorState(response) {
   // 错误处理
@@ -27,11 +33,8 @@ function successState(res) {
   // 正确处理
 }
 const httpServer = (opts, data, baseURL, token) => {
-
   // 公共参数
-  let publicOpts = {
-
-  };
+  let publicOpts = {};
 
   // http默认配置
   let httpDefaultOpts = {
@@ -41,16 +44,19 @@ const httpServer = (opts, data, baseURL, token) => {
     timeout: 10000,
     params: Object.assign(publicOpts, data),
     data: Object.assign(publicOpts, data),
-    headers: opts.method === 'get' ? {
-      'Authorization': `Bearer ${token}`,
-      'X-Requested-With': 'XMLHttpRequest',
-      'Accept': 'application/json',
-      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-    } : {
-      'Authorization': `Bearer ${token}`,
-      'X-Requested-With': 'XMLHttpRequest',
-      'Content-Type': 'application/json; charset=UTF-8'
-    }
+    headers:
+      opts.method === 'get'
+        ? {
+          Authorization: `Bearer ${token}`,
+          'X-Requested-With': 'XMLHttpRequest',
+          Accept: 'application/json',
+          'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        }
+        : {
+          Authorization: `Bearer ${token}`,
+          'X-Requested-With': 'XMLHttpRequest',
+          'Content-Type': 'application/json; charset=UTF-8'
+        }
   };
 
   if (opts.method === 'get') {
@@ -60,18 +66,15 @@ const httpServer = (opts, data, baseURL, token) => {
   }
 
   let promise = new Promise(function(resolve, reject) {
-    axios(httpDefaultOpts).then(
-      (res) => {
+    axios(httpDefaultOpts)
+      .then(res => {
         successState(res);
         resolve(res.data);
-      }
-    ).catch(
-      (response) => {
+      })
+      .catch(response => {
         errorState(response);
         reject(response);
-      }
-    );
-
+      });
   });
 
   return promise;
